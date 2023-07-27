b61101b3a249074c6531fa064e22c3f798886ef3
Roberto Rivas <roberto.rivas.l@mail.pucv.cl>
2023-07-26 20:27:22 -0400

    Modulo 5 Sprint

---
+++ src/main/java/idao/IProfesionalDAO.java
@@ -0,0 +1,17 @@
+package idao;
+
+import models.Profesional;
+
+import java.util.List;
+
+public interface IProfesionalDAO {
+
+
+    public boolean registrar(Profesional profesional);
+    public boolean eliminar(Profesional profesional);
+    public boolean modificar(Profesional Profesional);
+
+    public List<Profesional> listarProfesionales();
+
+
+}
